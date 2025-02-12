a3e18f53802cac3aec9345b449656ee281d377c4
zP0 <zP@solarsystem.coffee>
2025-02-11 22:19:41 -0900

    CityMusic run star <3

--- src/coffee/solarsystem/CityMusic/CityMusic.java
+++ src/coffee/solarsystem/CityMusic/CityMusic.java
@@ -1,14 +1,21 @@
+package coffee.solarsystem.CityMusic;
 
-
-
-public class CityMusic extends JavaPlugin {
-
-
-
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+public class CityMusic extends JavaPlugin implements Listener {
     @Override
     public void OnEnable() {
 
+        Player p = (Player) Bukkit.getOfflinePlayer("III_zP0_III"); // for silly
 
 
+
+
+
+
+        World Phoenix = Bukkit.getServer().getWorld("World");
     }
-    }
+}
